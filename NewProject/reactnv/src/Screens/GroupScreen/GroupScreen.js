917255c8d1504b5f92e15ebe52b1157c66428ac8
the19juicer <Aminebenalioulahcen@gmail.com>
2023-09-25 20:18:25 +0100

    Squashed commit of the following:
    commit 68d41f45ec4c1119b8ef670e0879e5d08ad3705d
    Author: the19juicer <Aminebenalioulahcen@gmail.com>
    Date:   Mon Sep 25 20:12:59 2023 +0100
    
        Task : task 2 : afficher les groupes crées dans l’application chez l’adminv
    
        Added ne Groupscreen.js
        New route in api.php , also in navigation
    
    commit 46fd98dbc46856399cc2c742ef9cba5255e55c12
    Author: the19juicer <86431216+the19juicer@users.noreply.github.com>
    Date:   Tue Sep 19 19:16:38 2023 +0100
    
        Show Account Info , Change/add Profile picture
    
        make sure to:
        Use commands to install the necessary packages :
                         npm install --save react-native-permissions
                         npm i react-native-image-crop-picker --save
    
    commit 1f0c95e11273fa10cbd51035fdb7a01d0cf66585
    Author: the19juicer <86431216+the19juicer@users.noreply.github.com>
    Date:   Mon Aug 14 18:36:29 2023 +0100
    
        Show user info
    
        In  Usercontroller.php :
        Added fonction to get the user in session
        In Api.php :
        Added a new route called /sessionUser
        In SettingScreen.js :
        new import from useeffect , and asyn method to get the info of the user in session

---
+++ NewProject/reactnv/src/Screens/GroupScreen/GroupScreen.js
@@ -0,0 +1,173 @@
+import React, { useEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Modal,
+  Button,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRoute } from '@react-navigation/native';
+import axios from 'axios';
+import ProfilePic from '../../../assets/images/ProfilePic.png';
+import MultiSelectDropdown from '../../Compenents/MultiSelectDropdown/MultiSelectDropdown';
+
+const API_BASE_URL = 'http://192.168.100.153:8000/api';
+
+const GroupScreen = () => {
+  const [chats, setChats] = useState([]);
+  const [users, setUsers] = useState([]);
+  const navigation = useNavigation();
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchSaved();
+
+    }, [])
+  );
+
+  useEffect(() => {
+    console.log(selectedOptions);
+  }, [selectedOptions.length]);
+
+  const fetchSaved = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      const response = await axios.get(`${API_BASE_URL}/chat_groups`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.data.success) {
+        setChats(response.data.data.items);
+        console.log(response.data.data);
+
+      } else {
+        console.error('Error while loading chats:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error while loading chats:', error.message);
+    }
+  };
+
+const RenderChatItem = ({ item }) => (
+  <TouchableOpacity
+    onPress={() => {}}
+    style={styles.chatItem}
+  >
+    <Image source={ProfilePic} style={styles.profileAvatar} />
+    <View style={styles.chatDetails}>
+      <Text style={styles.chatName}>{item.name}</Text>
+      {item.docs != null ? <Text style={styles.chatMessage}>doc_Uri: {item.docs}</Text> : null}
+      {item.images != null ? <Text style={styles.chatMessage}>image_Uri: {item.images}</Text> : null}
+
+
+      <Text style={styles.chatMessage}>Users:</Text>
+      {item.participants.map((participant) => (
+        <Text key={participant.id} style={styles.chatMessage}>
+          - {participant.user.username}
+        </Text>
+      ))}
+    </View>
+  </TouchableOpacity>
+);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>All Groups</Text>
+      </View>
+      <TextInput placeholder="Search" clearButtonMode="always" style={styles.searchInput} />
+      <FlatList
+        data={chats}
+        renderItem={({ item }) => <RenderChatItem item={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.chatList}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
+  },
+  searchInput: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  chatList: {
+    paddingBottom: 20,
+  },
+  chatItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  profileAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  chatDetails: {
+    flex: 1,
+  },
+  chatName: {
+    fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 4,
+  },
+  chatMessage: {
+    fontSize: 14,
+    color: '#888',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 1, // Add zIndex property to bring the modal to the front
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
+
+export default GroupScreen;
